ef7f88ca3ad620eb73b589cbd2817211caefdf94
ReinaldoJuunior <gamers.jrxiii@gmail.com>
2023-04-28 17:30:56 -0300

    restricting access and login

---
+++ routes/restrito.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => { res.send('restrito') })
+router.get('/noticias', (req, res) => res.send('noticias restrito'))
+
+
+module.exports = router
